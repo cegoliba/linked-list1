22fb5f1ecb4572d5f699d559b1452372cdc2ff56
cegoliba <cegoliba@gmail.com>
2014-12-08 19:44:21 +0200

    1

--- src/ilkuygulama/Ilkuygulama.java
+++ src/ilkuygulama/Ilkuygulama.java
@@ -1,26 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ilkuygulama;
-/**
- *
- * @author Cengiz
- */
+
+// @author cegoliba
 public class Ilkuygulama {
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("ne var la");
-        List lst=new List();
+
+        // list object
+        List lst = new List();
+
+        //build the list with nodes
         lst.Insert(5);
         lst.Insert(2);
         lst.Insert(3);
         lst.Insert(1);
         lst.Insert(4);
+
+        // print the nodes of the list
         lst.Display();
-    }    
+
+       // System.out.println(lst.toString());
+    }
+
 }
--- src/ilkuygulama/List.java
+++ src/ilkuygulama/List.java
@@ -1,34 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ilkuygulama;
-/**
- *
- * @author Cengiz
- */
+// @author cegoliba
 public class List {   
     
+    // Node structure
     class Item {
         int _value;
         Item _next;
         
+        // Constructor of the new node with new values
         Item(int value, Item next) {
             _value = value;
             _next = next;
         }        
     }
     
+    // Head of the list
     private Item list;
     
+    // Constructor of the empty list
     public List() {
         list = null;
     }
     
+    // Insert the new node into its own place in an ordered list
     public void Insert(int value) {
         Item temp = list;
         Item prev = list;
+        
+        // list is empty
         if (temp == null) {
             Item item = new Item(value, null);
             temp = item;
@@ -36,6 +35,7 @@ public class List {
             return;
         }
         
+        // the value of the first node is greater than the value of the new node
         if (value < temp._value) {
             Item item = new Item(value, list);
             temp = item;
@@ -43,24 +43,26 @@ public class List {
             return;
         }
         
+        // find the exact place of the node in the ordered list
         temp = temp._next;
         while (temp._next != null && temp._value > value) {
             temp = temp._next;
             prev = prev._next;
         }
         
+        // create and put the new node into its own order in the list
         Item item = new Item(value, null);
         prev._next = item;
-        item._next = temp;
-        
+        item._next = temp;        
     }
     
+    // Display the nodes of the list
     public void Display() {
         Item temp = list;
         while (temp != null) {
             System.out.print(temp._value + " ");
             temp=temp._next;
         }
-    }
+    }   
     
 }
